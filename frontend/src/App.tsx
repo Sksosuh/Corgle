bf82c936bb5f54a82ff17510dec32b2473b48993
Sksosuh <selsosuh@gmail.com>
2022-04-09 10:43:48 -0500

    add navbar comoponent
    Co-authored-by: Jack Kufa <jkufa@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Navbar from './components/Navbar/Navbar.component';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far, faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
+
+library.add(far, faQuestionCircle);
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
